2361f822dee9013e2fed90eea2a1d40f5619677c
salo345 <salahbenmer408@gmail.com>
2025-11-18 21:03:50 +0100

    Update solo_hub.lua

--- Games/solo_hub.lua
+++ Games/solo_hub.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/salo345/Solo-Library-ui-refs-heads-main/refs/heads/main/solo-V5-remake/menu.luau"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/salo345/Solo-Library-ui-refs-heads-main/refs/heads/main/solo-V5-remake/main.luau"))()
 
 
 local AFKOptions = {}
