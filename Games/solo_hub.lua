9e431c06fc536d2aa062ad30fa31d36f361ae865
salo345 <salahbenmer408@gmail.com>
2026-01-28 19:07:43 +0100

    Update solo_hub.lua

--- Games/solo_hub.lua
+++ Games/solo_hub.lua
@@ -1,24 +1,95 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Library/refs/heads/main/redz-V5-remake/main.luau"))()
+local sololib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Library/refs/heads/main/redz-V5-remake/main.luau"))()
 
-Library:SetUIScale(1.0)
-
-local Window = Library:MakeWindow({
-  Title = "Solo Hub : Bloxfruit",
+local Window = redzlib:MakeWindow({
+  Title = "Solo Hub : Blox Fruits",
   SubTitle = "by solo3333",
-  ScriptFolder = "redz-library-V5"
+  SaveFolder = "solobreak | redz lib v5.lua"
 })
 
-local Minimizer = Window:NewMinimizer({
-  KeyCode = Enum.KeyCode.LeftControl
+Window:AddMinimizeButton({
+    Button = { Image = "rbxassetid://71014873973869", BackgroundTransparency = 0 },
+    Corner = { CornerRadius = UDim.new(35, 1) },
 })
-
-local MobileButton = Minimizer:CreateMobileMinimizer({
-  Image = "rbxassetid://15298567397",
-  BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-})
-
---discord--
 
 local Discord = Window:MakeTab({"Discord", "Info"})
 
+Tab1:AddDiscordInvite({
+    Name = "Name Hub",
+    Description = "Join server",
+    Logo = "rbxassetid://18751483361",
+    Invite = "Link discord invite",
+})
 
+Window:SelectTab(Tab1)
+
+local Section = Tab1:AddSection({"Section"})
+
+local Paragraph = Tab1:AddParagraph({"Paragraph", "This is a Paragraph\nSecond Line"})
+
+  local Dialog = Window:Dialog({
+    Title = "Dialog",
+    Text = "This is a Dialog",
+    Options = {
+      {"Confirm", function()
+        
+      end},
+      {"Maybe", function()
+        
+      end},
+      {"Cancel", function()
+        
+      end}
+    }
+  })
+  
+  Tab1:AddButton({"Print", function(Value)
+print("Hello World!")
+end})
+
+local Toggle1 = Tab1:AddToggle({
+  Name = "Toggle",
+  Description = "This is a <font color='rgb(88, 101, 242)'>Toggle</font> Example",
+  Default = false 
+})
+Toggle1:Callback(function(Value)
+ 
+end)
+
+Tab1:AddToggle({
+    Name = "Toggle",
+    Default = false,
+    Callback = function(v)
+
+    end
+})
+
+Tab1:AddSlider({
+  Name = "Speed",
+  Min = 1,
+  Max = 100,
+  Increase = 1,
+  Default = 16,
+  Callback = function(Value)
+  
+  end
+})
+
+local Dropdown = Tab1:AddDropdown({
+  Name = "Players List",
+  Description = "Select the <font color='rgb(88, 101, 242)'>Number</font>",
+  Options = {"one", "two", "three"},
+  Default = "two",
+  Flag = "dropdown teste",
+  Callback = function(Value)
+    
+  end
+})
+
+Tab1:AddTextBox({
+  Name = "Name item",
+  Description = "1 Item on 1 Server", 
+  PlaceholderText = "item only",
+  Callback = function(Value)
+    
+  end
+})
